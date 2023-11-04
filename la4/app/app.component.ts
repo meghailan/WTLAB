c2d00e8a1da9f9bae1387de3490fce47b1123a3f
meghailan <122954422+meghailan@users.noreply.github.com>
2023-11-04 20:21:18 +0530

    Create app.component.ts

---
+++ la4/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+@Component({
+ selector: 'app-root',
+ standalone: true,
+ imports: [
+ HomeComponent,
+ RouterLink,
+ RouterOutlet,
+ ],
+ template: `
+ <main>
+ <a [routerLink]="['/']">
+ <header class="brand-name">
+ RESTAURANTS
+ </header>
+ </a>
+ <section class="content">
+ <router-outlet></router-outlet>
+ </section>
+ </main>
+ `,
+ styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+ title = 'homes';
+}
